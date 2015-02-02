b3f82718ffff05aca272f41525f41de8e855f02f
David MacMahon <davidm@astro.berkeley.edu>
2015-02-01 20:06:09 -0500

    Fix op_buffer blocks after X vector accumulators
    Simulation of the `op_buffer` blocks after the X accumulators revealed a
    problem whereby data samples were output multiple times.  This may be related
    to a missing divide-by-2 in the handling of their `n_x_op` mask parameter.
    
    This commit reimplements the op_buffer blocks using a FIFO rather than a dual
    ported BRAM and includes all new control circuitry.  This new implementation
    simulates as expected and should be compatible with the downstream logic that
    controls the readout of these op_buffer blocks.
    
    Also included is test model `x_vacc_test.mdl` and a run_x_vacc_test.m script
    that sets up and runs the simulation of the x_vacc_test model.  This test model
    includes both old (aka original) and new op_buffer buffer block
    implementations.

---
+++ src/run_x_vacc_test.m
@@ -0,0 +1,52 @@
+function [dout, vout, acc_end, acc_count, acc_count_pps, xdata, xvalid, xeof] = run_x_vacc_test()
+
+    baselines_per_channel = 20;
+    valids_per_channel = 11;
+    channels_per_dump = 4096/8;
+    dumps_per_acc = 2;
+    n_accs = 8;
+
+    din = zeros(baselines_per_channel, channels_per_dump, dumps_per_acc, n_accs);
+    vin = zeros(baselines_per_channel, channels_per_dump, dumps_per_acc, n_accs);
+
+    baseline_idx = 1;
+    din(baseline_idx, 1:channels_per_dump, 1, 1) = 1:channels_per_dump;
+    din(baseline_idx, 1:channels_per_dump, 1, 3) = channels_per_dump:-1:1;
+    vin(1:valids_per_channel,:,:,:) = 1;
+    
+    ws_data_in.time=(0:numel(din)-1)';
+    ws_data_in.signals.values=reshape(din,[],1);
+
+    ws_valid_in.time=(0:numel(vin)-1)';
+    ws_valid_in.signals.values=reshape(vin,[],1);
+
+    ws_dumps_per_acc_in.time=0;
+    ws_dumps_per_acc_in.signals.values=dumps_per_acc;
+
+    ws = get_param('x_vacc_test', 'ModelWorkspace');
+    ws.assignin('ws_data_in', ws_data_in);
+    ws.assignin('ws_valid_in', ws_valid_in);
+    ws.assignin('ws_dumps_per_acc_in', ws_dumps_per_acc_in);
+    
+    simout = sim('x_vacc_test', 'ReturnWorkspaceOutputs', 'on');
+    
+    dout          = simout.get('dout');
+    vout          = simout.get('vout');
+    acc_end       = simout.get('acc_end');
+    acc_count     = simout.get('acc_count');
+    acc_count_pps = simout.get('acc_count_pps');
+    xdata         = simout.get('xdata');
+    xvalid        = simout.get('xvalid');
+    xeof          = simout.get('xeof');
+
+    %idx=find(vout);
+    %expected = [4 6 12 14 36 38 16 18 24 26 28]';
+    %expected0 = [expected  ; expected+40; expected  ];
+    %expected1 = [expected+1; expected+41; expected+1];
+
+    %if all(acc0(idx) == expected0) && all(acc1(idx) == expected1)
+    %    fprintf('PASS: got expected output\n');
+    %else
+    %    fprintf('FAIL: did not get expected output\n');
+    %end
+end
